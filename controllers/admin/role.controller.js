fdccfc1708adc50221751ce8464537bfff04349b
HieuPhe <phehieu2003@gmail.com>
2025-10-17 15:24:44 +0700

    Xây dưng phần chỉnh sửa nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -25,9 +25,46 @@ module.exports.create = async (req, res) => {
 
 // [POST] /admin/roles/create
 module.exports.createPost = async (req, res) => {
-
   const record = new Role(req.body);
   await record.save();
 
   res.redirect(`${systemConfig.prefixAdmin}/roles`);
+};
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    let find = {
+      _id: id,
+      deleted: false,
+    };
+
+    const data = await Role.findOne(find);
+
+    res.render("admin/pages/roles/edit", {
+      pageTitle: "Sửa nhóm quyền",
+      data: data,
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/roles`);
+  }
+};
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await Role.updateOne({ _id: id }, req.body);
+    
+    req.flash("success", `Cập nhật thành công!`);
+
+  } catch (error) {
+
+    req.flash("error", `Cập nhật thất bại!`);
+  }
+
+  res.redirect(req.get("referer") || "/");
 };
