b9430a8a294555ec7d3a7b743a4de47b478b5bb8
HieuPhe <phehieu2003@gmail.com>
2025-10-24 21:32:39 +0700

    Trang danh danh sách sản phẩm bên client

--- controllers/client/project.controller.js
+++ controllers/client/project.controller.js
@@ -1,4 +1,7 @@
 const Project = require("../../models/project.model");
+const ProjectCategory = require("../../models/project-category.model");
+
+const projectCategoryHelper = require("../../helpers/project-category");
 
 // [GET] /client/projects
 module.exports.index = async (req, res) => {
@@ -19,7 +22,7 @@ module.exports.detail = async (req, res) => {
     const find = {
       deleted: false,
       slug: req.params.slug,
-      status: "OPEN"
+      status: "OPEN",
     };
 
     const project = await Project.findOne(find);
@@ -30,6 +33,35 @@ module.exports.detail = async (req, res) => {
     });
   } catch (error) {
     req.flash("error", `Không tồn tại công việc này!`);
+    res.redirect(`/projects`);
+  }
+};
+
+// [GET] /client/projects/:slugCategory
+module.exports.category = async (req, res) => {
+  try {
+    const category = await ProjectCategory.findOne({
+      slug: req.params.slugCategory,
+      status: "active",
+      deleted: false,
+    });
+
+    // lấy tất cả danh mục con
+    const listSubCategory = await projectCategoryHelper.getSubCategory(category.id);
+    
+    const listSubCategoryId = listSubCategory.map(item => item.id);
+
+    const projects = await Project.find({
+      project_category_id: { $in: [category.id, ...listSubCategoryId] }, 
+      deleted: false,
+    }).sort({ position: "desc" });
+
+    res.render("client/pages/projects/index", {
+      pageTitle: category.title,
+      projects: projects,
+    });
+  } catch (error) {
+    req.flash("error", `Không tồn tại danh mục này!`);
     res.redirect(`/projects`);
   }
 };
