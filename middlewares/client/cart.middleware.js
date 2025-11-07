3c76d2956fef842378dd2ef24afee64e9ffa9e2e
HieuPhe <phehieu2003@gmail.com>
2025-11-07 15:41:21 +0700

    nút miniCart

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -1,7 +1,6 @@
 const Cart = require("../../models/cart.model");
 
 module.exports.cartId = async (req, res, next) => {
-
   if (!req.cookies.cartId) {
     // tao gio hang
     const cart = new Cart();
@@ -14,6 +13,16 @@ module.exports.cartId = async (req, res, next) => {
     });
   } else {
     // lay ra thoi
+    const cart = await Cart.findOne({
+      _id: req.cookies.cartId,
+    });
+
+    // const totalProject = cart.projects.reduce((sum, item) => sum + item, 0);
+
+    cart.totalProject = cart.projects.length;
+    
+    res.locals.miniCart = cart;
+    
   }
 
   next();
